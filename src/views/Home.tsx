8ba9917b24fc309d73488e0264ec8d7b5c34798c
PatrickPahunang <patrickcd452@gmail.com>
2024-05-25 20:26:38 +0800

    tested

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faEnvelope);
+
 const Home: React.FC = () => {
   return (
     <>
-      <div className="flex items-center justify-center text-7xl text-red-500 w-screen h-screen font-black">HOME TESTING</div>
+      <div className="flex xl:flex-row flex-col  items-center justify-center  w-screen h-screen  overflow-hidden">
+        <div className="xl:w-1/2 w-full flex flex-col items-center justify-center  h-full xl:p-10 p-3 gap-4">
+          <div className=" w-full flex items-center justify-start xl:text-6xl text-3xl font-black" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="300">
+            Find the Best, Shop the Rest – Only at GoodFinds!
+          </div>
+          <div className=" w-full flex items-center justify-start font-medium xl:text-2xl text-md" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500">
+            Shop Smart, Shop Stylish , Live Better – Find Your Favorites Online!
+          </div>
+          <div className=" w-full flex items-center justify-start gap-4" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="700">
+            <div className="xl:w-1/2 w-full relative">
+              <input type="email" className=" p-4 rounded-xl pl-12 w-full border-2 border-gray-300" placeholder="Enter your email" />
+              <FontAwesomeIcon icon={faEnvelope} className="absolute text-2xl left-3 top-4 text-gray-500" />
+            </div>
+
+            <button className="p-4 px-8 rounded-xl bg-blue-800 text-white xl:w-1/5 w-1/2">Subscribe</button>
+          </div>
+        </div>
+
+        <div className="xl:w-1/2 w-full flex items-center justify-center  xl:h-full h-max" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="700">
+          <img src="../../src/assets/images/front2.jpg" className="xl:h-[90%] h-60  xl:w-full w-60 object-cover object-bottom rounded-md" />
+        </div>
+      </div>
     </>
   );
 };
--- src/views/Navbar.tsx
+++ src/views/Navbar.tsx
@@ -3,18 +3,38 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="w-screen bg-slate-950 flex items-center justify-center gap-6 p-6 text-xl transition duration-500">
-      <Link to="/" className=" hover:text-white border-b-2 border-transparent text-cyan-500 hover:border-white px-8 py-2">
-        Home
-      </Link>
+    <nav className="border-gray-200 bg-gray-100 w-screen" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="../../src/assets/images/logo.png" className="w-20 object-cover mix-blend-multiply" />
+          {/* <span className="self-center text-2xl font-semibold whitespace-nowrap text-slate-950">GoodFinds</span> */}
+        </a>
+        <button
+          data-collapse-toggle="navbar-solid-bg"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-solid-bg"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto text-xl" id="navbar-solid-bg">
+          <Link to="/" className=" text-slate-950 hover:text-cyan-500 border-b-2 border-transparent  hover:border-cyan-500 px-8 py-2">
+            Home
+          </Link>
 
-      <Link to="/AboutUS" className=" hover:text-white border-b-2 border-transparent text-cyan-500 hover:border-white px-8 py-2">
-        About Us
-      </Link>
+          <Link to="/AboutUS" className=" text-slate-950 hover:text-cyan-500 border-b-2 border-transparent  hover:border-cyan-500 px-8 py-2">
+            About Us
+          </Link>
 
-      <Link to="/Products" className=" hover:text-white border-b-2 border-transparent text-cyan-500 hover:border-white px-8 py-2">
-        Products
-      </Link>
+          <Link to="/Products" className=" text-slate-950 hover:text-cyan-500 border-b-2 border-transparent  hover:border-cyan-500 px-8 py-2">
+            Products
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 }
